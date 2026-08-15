c788acc4c8e8883889e9df7da7fcc6fa93d4660c
Tuan Anh <tuan73176@gmail.com>
2026-08-15 16:40:34 +0000

    sm8450-common: Update system WFD blobs to nezha OS3.0.332.0.XPAMIXM
    Change-Id: I1cc02a7e6e949699992194413c660874064abbfe

--- extract-files.py
+++ extract-files.py
@@ -48,6 +48,9 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/etc/seccomp_policy/tcmd.policy': blob_fixup()
         .add_line_if_missing('lseek: 1'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudiobase.so')
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
         .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
     'vendor/bin/qcc-trd': blob_fixup()
